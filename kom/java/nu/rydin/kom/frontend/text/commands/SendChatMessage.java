4b1b54d230d7cc3c3a6170669b9eba52effcf42e
jepson <>
2004-06-06 21:09:14 +0000

    Rewritten.

--- kom/java/nu/rydin/kom/frontend/text/commands/SendChatMessage.java
+++ kom/java/nu/rydin/kom/frontend/text/commands/SendChatMessage.java
@@ -11,9 +11,9 @@ import java.io.PrintWriter;
 
 import nu.rydin.kom.KOMException;
 import nu.rydin.kom.MissingArgumentException;
-import nu.rydin.kom.backend.NameUtils;
+import nu.rydin.kom.ObjectNotFoundException;
 import nu.rydin.kom.backend.ServerSession;
-import nu.rydin.kom.backend.data.UserManager;
+import nu.rydin.kom.backend.data.ObjectManager;
 import nu.rydin.kom.frontend.text.AbstractCommand;
 import nu.rydin.kom.frontend.text.Context;
 import nu.rydin.kom.frontend.text.LineEditor;
@@ -38,32 +38,15 @@ public class SendChatMessage extends AbstractCommand
 		if(parameters.length == 0)
 			throw new MissingArgumentException();
 		
-		// Special case: * sends to all
-		//
-		long user = "*".equals(parameters[0])
-			? -1
-			: NamePicker.resolveName(NameUtils.assembleName(parameters), UserManager.USER_KIND, context);
-
 		// Set up
 		//
 		LineEditor in = context.getIn();
 		PrintWriter out = context.getOut();
 		ServerSession session = context.getSession();
 		String me = session.getLoggedInUser().getName();
-		
+				
 		// Print prompt
 		//
-		if (user == -1)
-		{
-		    //Special case, sending to all users
-		    out.println(context.getMessageFormatter().format("chat.saytoall"));
-		}
-		else
-		{
-		    //Print username
-		    out.println(context.getMessageFormatter().format("chat.saytouser", session.getName(user)));
-		}
-	    out.println("");
 		out.print(me);
 		out.print(": ");
 		out.flush();
@@ -81,10 +64,40 @@ public class SendChatMessage extends AbstractCommand
 		
 		// Send it
 		//
-		if(user == -1)
+		if("*".equals(parameters[0]))
 			session.broadcastChatMessage(message);
 		else
-			session.sendChatMessage(user, message);
+		{
+			// TODO: Handle single recipients separately here (most common case and cheaper to do).
+
+			// Now splice the parameters and tokenize them again.
+			//
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < parameters.length; ++i)
+			{
+				sb.append(parameters[i]).append(" ");
+			}
+			String[] uarray = sb.toString().trim().split(",");
+			
+			// Resolve each name to an ID.
+			//
+			long[] destinations = new long[uarray.length];
+			for (int i = 0; i < uarray.length; ++i)
+			{
+				try
+				{
+					destinations[i] = NamePicker.resolveName(uarray[i], ObjectManager.UNKNOWN_KIND, context);
+				}
+				catch (ObjectNotFoundException e)
+				{
+					destinations[i] = -1;
+				}
+			}
+			
+			// Finally call the multicast handler.
+			//
+			session.sendMulticastMessage(destinations, message);
+		}
 	}
 
 	public boolean acceptsParameters()
