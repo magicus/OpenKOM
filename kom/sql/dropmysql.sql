550dc269f1214f34b0d2083b3fee160a20bbab88
skrolle <>
2004-07-16 18:22:26 +0000

    added delete messagesearch

--- kom/sql/dropmysql.sql
+++ kom/sql/dropmysql.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS magicconferences;
 DROP TABLE IF EXISTS messageattributes;
 DROP TABLE IF EXISTS messageoccurrences;
 DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS messagesearch;
 DROP TABLE IF EXISTS memberships;
 DROP TABLE IF EXISTS conferences;
 DROP TABLE IF EXISTS messagelogpointers;
