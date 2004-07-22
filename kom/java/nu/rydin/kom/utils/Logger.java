818e9044450964a8e62c77a716e4f196152c6553
skrolle <>
2004-07-22 10:12:46 +0000

    Changed logging config file to use auto-loaded log4j.properties instead.

--- kom/java/nu/rydin/kom/utils/Logger.java
+++ kom/java/nu/rydin/kom/utils/Logger.java
@@ -6,14 +6,6 @@
  */
 package nu.rydin.kom.utils;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.xml.DOMConfigurator;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
 /**
  * @author Henrik Schröder
  */
@@ -36,40 +28,10 @@ public class Logger extends Object
 
 	public static void initialize() throws Exception
 	{
-		//The name of the logging config file shall be log.xml, log.xml
-		//is the name of the config file, the whole config file and
-		//nothing but the config file, woe unto thee if there be no
-		//config file named thusly, and woe unto your children, and your
-		//children's children, and you children's children's children and so on
-		//and so forth for seven and one generations or until you don't think
-		//it's very funny anymore.
-
-		//TODO: TRÖTT I NÄSAN. ORKAR INTE FIXA JUST NU.
-		DOMConfigurator.configureAndWatch("D:/eclipse/workspace/kom/java/log.xml");
-
-		/*
-		 // First, see if there are any appenders on the root category.
-		 //
-		 Enumeration appenders = org.apache.log4j.Logger.getRoot().getAllAppenders();
-		 if(!(appenders instanceof NullEnumeration))
-		 {
-		 // Using already-supplied configuration
-		 return;
-		 }
-
-		 // Create a hardcoded default configuration.
-		 //
-		 Document document = Logger.createDefaultConfiguration();
-
-		 if(document != null)
-		 {
-		 DOMConfigurator.configure(document.getDocumentElement());
-		 }
-		 else
-		 {
-		 BasicConfigurator.configure();
-		 }
-		 */
+		//Never mind, we're just relying on log4j's ability to autoload log4j.properties.
+		
+		//TODO: Maybe we should add the ability to read from a specified XML config file
+		//DOMConfigurator.configureAndWatch("D:/eclipse/workspace/kom/java/log.xml");
 	}
 	
 	public static boolean isDebugEnabled(Object invoker)
@@ -183,53 +145,5 @@ public class Logger extends Object
 				: invoker.getClass();
 		return (org.apache.log4j.Logger)org.apache.log4j.Logger
 				.getInstance(invokerClass.getName());
-	}
-
-	private static Document createDefaultConfiguration() throws Exception
-	{
-		DocumentBuilder db = DocumentBuilderFactory.newInstance()
-				.newDocumentBuilder();
-		Document defaultConfiguration = db.newDocument();
-
-		Element element = defaultConfiguration.createElementNS(
-				"http://jakarta.apache.org/log4j/", "log4j:configuration");
-
-		defaultConfiguration.appendChild(element);
-		Element appender = defaultConfiguration.createElement("appender");
-		Element layout = defaultConfiguration.createElement("layout");
-		Element category = defaultConfiguration.createElement("category");
-		Element root = defaultConfiguration.createElement("root");
-
-		Element param = defaultConfiguration.createElement("param");
-		appender.setAttribute("name", "STDOUT");
-		appender.setAttribute("class", ConsoleAppender.class.getName());
-
-		param = defaultConfiguration.createElement("param");
-		appender.appendChild(param);
-		param.setAttribute("name", "Target");
-		param.setAttribute("value", ConsoleAppender.SYSTEM_OUT);
-
-		appender.appendChild(layout);
-		layout.setAttribute("class", "org.apache.log4j.PatternLayout");
-
-		param = defaultConfiguration.createElement("param");
-		layout.appendChild(param);
-		param.setAttribute("value", "[%-5p][%c] %x%m%n");
-		param.setAttribute("name", "ConversionPattern");
-
-		Element priority = defaultConfiguration.createElement("priority");
-		root.appendChild(priority);
-		priority.setAttribute("value", "warn");
-
-		Element appenderRef = defaultConfiguration
-				.createElement("appender-ref");
-		root.appendChild(appenderRef);
-		appenderRef.setAttribute("ref", "STDOUT");
-
-		element.appendChild(appender);
-		element.appendChild(category);
-		element.appendChild(root);
-
-		return defaultConfiguration;
 	}
 }
