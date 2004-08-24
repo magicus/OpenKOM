a249321236de061e068fb0e39135f0f4725c0282
skrolle <>
2004-08-24 13:22:00 +0000

    Split chatmessage command into two: One for broadcastmessages and one for private messages.

---
+++ kom/java/nu/rydin/kom/frontend/text/commands/SendBroadcastChatMessage.java
@@ -0,0 +1,99 @@
+/*
+ * Created on Aug 24, 2004
+ *
+ * Distributed under the GPL license.
+ * See http://www.gnu.org for details
+ */
+package nu.rydin.kom.frontend.text.commands;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import nu.rydin.kom.backend.ServerSession;
+import nu.rydin.kom.exceptions.KOMException;
+import nu.rydin.kom.frontend.text.AbstractCommand;
+import nu.rydin.kom.frontend.text.Context;
+import nu.rydin.kom.frontend.text.DisplayController;
+import nu.rydin.kom.frontend.text.editor.WordWrapper;
+import nu.rydin.kom.frontend.text.editor.simple.AbstractEditor;
+import nu.rydin.kom.frontend.text.editor.simple.SimpleChatEditor;
+import nu.rydin.kom.frontend.text.parser.CommandLineParameter;
+import nu.rydin.kom.frontend.text.parser.RawParameter;
+import nu.rydin.kom.i18n.MessageFormatter;
+import nu.rydin.kom.structs.NameAssociation;
+
+/**
+ * @author Henrik Schröder
+ */
+public class SendBroadcastChatMessage extends AbstractCommand {
+
+    public SendBroadcastChatMessage(Context context, String fullName) {
+        super(fullName, new CommandLineParameter[] { new RawParameter("chat.saytoall", false) });
+    }
+
+    public void execute(Context context, Object[] parameterArray)
+            throws KOMException, IOException, InterruptedException {
+
+        // Set up
+        //
+        DisplayController dc = context.getDisplayController();
+        PrintWriter out = context.getOut();
+        ServerSession session = context.getSession();
+		MessageFormatter formatter = context.getMessageFormatter();
+
+		String message;
+        
+        if (parameterArray[0] == null)
+        {
+            //No message given, use chat message editor.
+            dc.normal();
+            out.println(context.getMessageFormatter().format("chat.saytoall"));
+            out.flush();
+
+            // Read message
+            //
+            AbstractEditor editor = new SimpleChatEditor(context
+                    .getMessageFormatter());
+            message = editor.edit(context, -1).getBody();
+        }
+        else
+        {
+            message = (String)parameterArray[0];
+        }
+        
+
+        // Empty message? User interrupted
+        //
+        if (message.length() == 0)
+            return;
+
+		// Send it
+		//
+		NameAssociation[] refused = session.broadcastChatMessage(message);
+		
+		// Print refused destinations (if any)
+		//
+		int top = refused.length;
+		if(top > 0)
+		{
+		    // Build message
+		    //
+		    StringBuffer sb = new StringBuffer(200);
+		    sb.append(formatter.format("chat.refused"));
+		    for(int idx = 0; idx < top; ++idx)
+		    {
+		        sb.append(refused[idx].getName());
+		        if(idx < top - 1)
+		            sb.append(", ");
+		    } 
+		    
+		    // Wordwrap it!
+		    //
+		    out.println();
+		    WordWrapper ww = context.getWordWrapper(sb.toString());
+		    String line = null;
+		    while((line = ww.nextLine()) != null)
+		        out.println(line);
+		}
+    }
+}
--- kom/java/nu/rydin/kom/frontend/text/commands/SendChatMessage.java
+++ kom/java/nu/rydin/kom/frontend/text/commands/SendChatMessage.java
@@ -1,7 +1,7 @@
 /*
  * Created on Nov 11, 2003
  *
- * Distributed under the GPL licens.
+ * Distributed under the GPL license.
  * See http://www.gnu.org for details
  */
 package nu.rydin.kom.frontend.text.commands;
@@ -64,82 +64,73 @@ public class SendChatMessage extends AbstractCommand
 		//Parse parameters to get list of recipients.
 		//
 		long[] destinations = null;
-		if("*".equals(parameters[0]))
+
+		// TODO: Handle single recipients separately here (most common case and cheaper to do).
+
+		// Now splice the parameters and tokenize them again.
+		//
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < parameters.length; ++i)
 		{
-		    //Print beginning of prompt for message to all users.
-			dc.normal();
-		    out.println(context.getMessageFormatter().format("chat.saytoall"));
+			sb.append(parameters[i]).append(" ");
 		}
-		else
-		{
-			// TODO: Handle single recipients separately here (most common case and cheaper to do).
-
-			// Now splice the parameters and tokenize them again.
-			//
-			StringBuffer sb = new StringBuffer();
-			for (int i = 0; i < parameters.length; ++i)
-			{
-				sb.append(parameters[i]).append(" ");
-			}
-			String[] uarray = sb.toString().trim().split(",");
-			
-			// Resolve each name to an ID.
-			//
-			destinations = new long[uarray.length];
-			String recipients = "";
-			for (int i = 0; i < uarray.length; ++i)
-			{
-			    long id = NamePicker.resolveNameToId(uarray[i], NameManager.UNKNOWN_KIND, context);
-				destinations[i] = id;
-				recipients += ", " + session.getName(id);
-			}
-
-			// Check that all users are logged in an can receive messages
-			//
-			StringWriter sw = new StringWriter();
-			PrintWriter pw = new PrintWriter(sw);
-			boolean error = false;
-			int[] status = session.verifyChatRecipients(destinations);
-			int top = status.length;
-			for(int idx = 0; idx < top; ++idx)
-			{
-			    long each = destinations[idx];
-			    switch(status[idx])
-			    {
-			    	case ChatRecipientStatus.NONEXISTENT:
-			    	    pw.println(formatter.format("chat.nonexistent", session.getName(each)));
-			    		error = true; 
-			    		break;
-			    	case ChatRecipientStatus.NOT_LOGGED_IN:
-			    	    pw.println(formatter.format("chat.not.logged.in", session.getName(each)));
-			    		error = true;
-			    		break;
-			    	case ChatRecipientStatus.REFUSES_MESSAGES:
-			    	    pw.println(formatter.format("chat.refuses.messages", session.getName(each)));
-			    		error = true;	
-			    		break;			    	
-			    	case ChatRecipientStatus.OK_CONFERENCE:
-			    	case ChatRecipientStatus.OK_USER:
-			    	    break;
-			    	default:
-			    	    throw new UnexpectedException(context.getLoggedInUserId());
-			    }
-			}
-			if(error)
-			    throw new KOMUserException(sw.toString());
+		String[] uarray = sb.toString().trim().split(",");
 		
-			//Print beginning of prompt for message to a list of users
-			//
-			if (recipients.length() > 2)
-			{
-			    recipients = recipients.substring(2);
-			}
-			dc.normal();
-		    out.println(context.getMessageFormatter().format("chat.saytouser", recipients));
-
+		// Resolve each name to an ID.
+		//
+		destinations = new long[uarray.length];
+		String recipients = "";
+		for (int i = 0; i < uarray.length; ++i)
+		{
+		    long id = NamePicker.resolveNameToId(uarray[i], NameManager.UNKNOWN_KIND, context);
+			destinations[i] = id;
+			recipients += ", " + session.getName(id);
 		}
+
+		// Check that all users are logged in an can receive messages
+		//
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		boolean error = false;
+		int[] status = session.verifyChatRecipients(destinations);
+		int top = status.length;
+		for(int idx = 0; idx < top; ++idx)
+		{
+		    long each = destinations[idx];
+		    switch(status[idx])
+		    {
+		    	case ChatRecipientStatus.NONEXISTENT:
+		    	    pw.println(formatter.format("chat.nonexistent", session.getName(each)));
+		    		error = true; 
+		    		break;
+		    	case ChatRecipientStatus.NOT_LOGGED_IN:
+		    	    pw.println(formatter.format("chat.not.logged.in", session.getName(each)));
+		    		error = true;
+		    		break;
+		    	case ChatRecipientStatus.REFUSES_MESSAGES:
+		    	    pw.println(formatter.format("chat.refuses.messages", session.getName(each)));
+		    		error = true;	
+		    		break;			    	
+		    	case ChatRecipientStatus.OK_CONFERENCE:
+		    	case ChatRecipientStatus.OK_USER:
+		    	    break;
+		    	default:
+		    	    throw new UnexpectedException(context.getLoggedInUserId());
+		    }
+		}
+		if(error)
+		    throw new KOMUserException(sw.toString());
+	
+		//Print beginning of prompt for message to a list of users
+		//
+		if (recipients.length() > 2)
+		{
+		    recipients = recipients.substring(2);
+		}
+		dc.normal();
+	    out.println(context.getMessageFormatter().format("chat.saytouser", recipients));
+
 		out.flush();
-		
 		
 		// Read message
 		//
@@ -156,30 +147,28 @@ public class SendChatMessage extends AbstractCommand
 		//
 		// Can't make it less ugly than this...
 		//
-		NameAssociation[] refused = "*".equals(parameters[0])
-			? session.broadcastChatMessage(message)
-			: session.sendMulticastMessage(destinations, message);
+		NameAssociation[] refused = session.sendMulticastMessage(destinations, message);
 		
 		// Print refused destinations (if any)
 		//
-		int top = refused.length;
-		if(top > 0)
+		int top2 = refused.length;
+		if(top2 > 0)
 		{
 		    // Build message
 		    //
-		    StringBuffer sb = new StringBuffer(200);
-		    sb.append(formatter.format("chat.refused"));
-		    for(int idx = 0; idx < top; ++idx)
+		    StringBuffer sb2 = new StringBuffer(200);
+		    sb2.append(formatter.format("chat.refused"));
+		    for(int idx = 0; idx < top2; ++idx)
 		    {
-		        sb.append(refused[idx].getName());
-		        if(idx < top - 1)
-		            sb.append(", ");
+		        sb2.append(refused[idx].getName());
+		        if(idx < top2 - 1)
+		            sb2.append(", ");
 		    } 
 		    
 		    // Wordwrap it!
 		    //
 		    out.println();
-		    WordWrapper ww = context.getWordWrapper(sb.toString());
+		    WordWrapper ww = context.getWordWrapper(sb2.toString());
 		    String line = null;
 		    while((line = ww.nextLine()) != null)
 		        out.println(line);
