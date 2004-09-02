3eb7611a72ae80a6b0092b6be6e5febcecfa77b0
ihse <>
2004-09-02 18:07:05 +0000

    adding timezone parameter.

--- kom/java/nu/rydin/kom/frontend/text/commands/ChangeTimeZone.java
+++ kom/java/nu/rydin/kom/frontend/text/commands/ChangeTimeZone.java
@@ -7,20 +7,15 @@
 package nu.rydin.kom.frontend.text.commands;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.TimeZone;
 
-import nu.rydin.kom.exceptions.InvalidChoiceException;
 import nu.rydin.kom.exceptions.KOMException;
-import nu.rydin.kom.exceptions.OperationInterruptedException;
 import nu.rydin.kom.frontend.text.AbstractCommand;
 import nu.rydin.kom.frontend.text.Context;
 import nu.rydin.kom.frontend.text.KOMWriter;
 import nu.rydin.kom.frontend.text.LineEditor;
 import nu.rydin.kom.frontend.text.parser.CommandLineParameter;
-import nu.rydin.kom.frontend.text.parser.Parser;
-import nu.rydin.kom.frontend.text.parser.RawParameter;
+import nu.rydin.kom.frontend.text.parser.TimeZoneParameter;
 import nu.rydin.kom.i18n.MessageFormatter;
 
 /**
@@ -30,8 +25,7 @@ public class ChangeTimeZone extends AbstractCommand
 {
     public ChangeTimeZone(Context context, String fullName)
     {
-        super(fullName, new CommandLineParameter[] { new RawParameter("change.timezone.prompt", true) });
-    }
+        super(fullName, new CommandLineParameter[] { new TimeZoneParameter(true) });    }
 
     public void execute(Context context, Object[] parameters)
             throws KOMException, IOException, InterruptedException
@@ -42,23 +36,7 @@ public class ChangeTimeZone extends AbstractCommand
         
         // Get timezone string
         //
-        String tz = (String) parameters[0];
-        if(tz.length() == 0)
-            throw new OperationInterruptedException();
-        
-        // Try to match with existing timezones.
-        //
-		String[] matches = this.matchTimezone(tz);
-        int top = matches.length;
-        
-        // No match? Error!
-        //
-        if(top == 0)
-            throw new InvalidChoiceException();
-        
-        // Resolve ambiguities (if any)
-        //
-        String match = top == 1 ? matches[0] : this.resolveAmbiguities(context, matches);
+        String match = TimeZoneParameter.getJavaNameForTimeZoneSelection((Integer) parameters[0]);
         
         // Update database and clear cache.
         //
@@ -70,37 +48,5 @@ public class ChangeTimeZone extends AbstractCommand
         //
         out.println();
         out.println(formatter.format("change.timezone.confirmation", match));
-    }
-
-	private String resolveAmbiguities(Context context, String[] matches)
-	throws InvalidChoiceException, OperationInterruptedException, IOException, InterruptedException
-	{
-		List matchList = new ArrayList(matches.length);
-		
-		for (int i = 0; i < matches.length; i++) {
-			matchList.add(matches[i]);
-		}
-		int selection = Parser.askForResolution(context, matchList, "change.timezone.pick.prompt", true, "change.timezone.ambiguous");
-		return matches[selection];
-	}
-    
-    private String[] matchTimezone(String key)
-    {
-        key = key.toUpperCase().replace(' ', '_');
-        ArrayList list = new ArrayList();
-        String[] timeZones = TimeZone.getAvailableIDs();
-        int top = timeZones.length;
-        for(int idx  = 0; idx < top; ++idx)
-        {
-            String each = timeZones[idx];
-            String candidate = each.toUpperCase();
-            if(candidate.indexOf(key) != -1)
-                list.add(each);
-            if(candidate.equals(key))
-                return new String[] { each };
-        }
-        String[] result = new String[list.size()];
-        list.toArray(result);
-        return result;
     }
 }
---
+++ kom/java/nu/rydin/kom/frontend/text/parser/TimeZoneParameter.java
@@ -0,0 +1,58 @@
+/*
+ * Created on 2004-aug-19
+ * 
+ * Distributed under the GPL license.
+ * See http://www.gnu.org for details
+ */
+package nu.rydin.kom.frontend.text.parser;
+
+import java.util.TimeZone;
+
+
+/**
+ * @author Magnus Ihse 
+ */
+public class TimeZoneParameter extends EnumParameter
+{
+	private static String[] m_javaTimeZones;
+	private static String[] m_presentableTimeZones;
+
+	static {
+    	m_javaTimeZones = TimeZone.getAvailableIDs();
+    	m_presentableTimeZones = convertToPresentable(m_javaTimeZones);
+	}
+	
+    public TimeZoneParameter(String missingObjectQuestionKey, boolean isRequired)
+    {
+        super(missingObjectQuestionKey, "parser.parameter.timezone.header", missingObjectQuestionKey, m_presentableTimeZones, isRequired);
+    }
+    
+    /**
+	 * @param timeZones
+	 * @return
+	 */
+	private static String[] convertToPresentable(String[] timeZones) {
+		String[] converted = new String[timeZones.length];
+		
+		for (int i = 0; i < timeZones.length; i++) {
+			String original = timeZones[i];
+			converted[i] = original.replace('/', ' ').replace('_', ' ');
+		}
+		
+		return converted;
+	}
+
+	public static String getJavaNameForTimeZoneSelection(Integer selection) {
+		return m_javaTimeZones[selection.intValue()];
+	}
+	
+	public TimeZoneParameter(boolean isRequired)
+    {
+        this("parser.parameter.timezone.ask", isRequired);
+    }
+
+    protected String getUserDescriptionKey()
+    {
+        return "parser.parameter.timezone.description";
+    }
+}
