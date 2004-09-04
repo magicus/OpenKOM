b2c54b2dcd6c2229d1869bf6ba0a22067d1cbd8b
skrolle <>
2004-09-04 17:33:25 +0000

    Added show license

---
+++ kom/java/nu/rydin/kom/frontend/text/commands/ShowLicense.java
@@ -0,0 +1,30 @@
+/*
+ * Created on Nov 7, 2003
+ *
+ * Distributed under the GPL license.
+ * See http://www.gnu.org for details
+ */
+package nu.rydin.kom.frontend.text.commands;
+
+import java.io.PrintWriter;
+
+import nu.rydin.kom.frontend.text.AbstractCommand;
+import nu.rydin.kom.frontend.text.Context;
+
+/**
+ * @author <a href=mailto:pontus@rydin.nu>Pontus Rydin</a>
+ */
+public class ShowLicense extends AbstractCommand
+{
+	public ShowLicense(Context context, String fullName)
+	{
+		super(fullName, AbstractCommand.NO_PARAMETERS);	
+	}
+
+	public void execute(Context context, Object[] parameterArray)
+	{
+	    PrintWriter out = context.getOut();
+	    out.println(context.getMessageFormatter().format("license.text1"));
+	    out.println(context.getMessageFormatter().format("license.text2"));
+	}
+}
