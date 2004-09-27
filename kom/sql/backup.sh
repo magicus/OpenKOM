25f8f153395855dd05e2ebdb1724ae7846d215f0
skrolle <>
2004-09-27 14:31:23 +0000

    Backup script.

---
+++ kom/sql/backup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mydate=`date +%Y%m%d`
+myfile=backup_$mydate.sql 
+echo SET AUTOCOMMIT=0; > $myfile.sql
+echo SET FOREIGN_KEY_CHECKS=0; >> $myfile
+mysqldump -u root --opt --skip-lock-tables --single-transaction kom conferences files magicconferences memberships messageattributes messagelog messagelogpointers messageoccurrences messages names settings userlog users >> $myfile
+echo SET FOREIGN_KEY_CHECKS=1; >> $myfile
+echo COMMIT; >> $myfile
+echo SET AUTOCOMMIT=1; >> $myfile
+echo DELETE FROM messagesearch\; >> $myfile
+echo INSERT INTO messagesearch SELECT id, subject, body FROM messages\; >> $myfile
