f30ce4ddb164fc8b005dc2fba250fb6469a3b615
skrolle <>
2004-09-29 23:13:44 +0000

    Changed behaviour of MoveMessage to only move original occurrence. Fixed confirmation message.

--- kom/java/nu/rydin/kom/frontend/text/commands/MoveMessage.java
+++ kom/java/nu/rydin/kom/frontend/text/commands/MoveMessage.java
@@ -10,11 +10,15 @@ import java.io.PrintWriter;
 
 import nu.rydin.kom.backend.ServerSession;
 import nu.rydin.kom.exceptions.KOMException;
+import nu.rydin.kom.exceptions.NoCurrentMessageException;
+import nu.rydin.kom.exceptions.ObjectNotFoundException;
 import nu.rydin.kom.frontend.text.AbstractCommand;
 import nu.rydin.kom.frontend.text.Context;
+import nu.rydin.kom.frontend.text.DisplayController;
 import nu.rydin.kom.frontend.text.parser.CommandLineParameter;
 import nu.rydin.kom.frontend.text.parser.ConferenceParameter;
 import nu.rydin.kom.i18n.MessageFormatter;
+import nu.rydin.kom.structs.MessageOccurrence;
 import nu.rydin.kom.structs.NameAssociation;
 
 /**
@@ -31,20 +35,54 @@ public class MoveMessage extends AbstractCommand
 	public void execute(Context context, Object[] parameterArray)
 	throws KOMException
 	{
-	    NameAssociation nameAssociation = (NameAssociation) parameterArray[0];
-		
-		long conference = nameAssociation.getId();
-		ServerSession ss = context.getSession();
-		int localNum = ss.globalToLocalInConference(ss.getCurrentConferenceId(), 
-													ss.getLastMessageHeader().getId()).getLocalnum();
-		
-		// MOVE ZIG!!
-		//
-		context.getSession().moveMessage(localNum, conference);
-
+	    ServerSession session = context.getSession();
 		PrintWriter out = context.getOut();
 		MessageFormatter fmt = context.getMessageFormatter();
-		out.println(fmt.format("move.confirmation", 
-			new Object [] { new Long(localNum), context.getSession().getName(conference) } ));	
+	    DisplayController dc = context.getDisplayController();
+	    
+	    dc.normal();
+		
+		long messageid = session.getCurrentMessage();
+		if(messageid == -1)
+			throw new NoCurrentMessageException();
+	    
+	    long destination = ((NameAssociation)parameterArray[0]).getId();
+		
+	    try
+        {
+            // Retrieve current original occurrence to try and get the local number
+            //
+            MessageOccurrence original = session
+                    .getOriginalMessageOccurrence(messageid);
+            int localnum = -1;
+            if (original.getConference() == session.getCurrentConferenceId())
+            {
+                //We're moving a message whose original is in the current conference...
+                localnum = original.getLocalnum();
+            }
+
+            // MOVE ZIG!!
+            //
+            session.moveMessage(messageid, destination);
+            
+            if (localnum == -1)
+            {
+                //Print global confirmation
+        		out.println(fmt.format("move.global.confirmation", 
+        				new Object [] { new Long(messageid), session.getName(destination) } ));	
+            }
+            else
+            {
+                //Print local confirmation 
+        		out.println(fmt.format("move.local.confirmation", 
+        				new Object [] { new Long(localnum), session.getName(destination) } ));	
+            }
+        } 
+	    catch (ObjectNotFoundException e)
+        {
+            //Explicitly handle this exception here since we want a custom error message.
+	        //FIXME: Make a custom exception for this error and move error message to the exception error printer thing. 
+	        out.println(fmt.format("move.failure"));
+        }
 	}
 }
