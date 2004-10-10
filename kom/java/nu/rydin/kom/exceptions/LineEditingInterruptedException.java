778fdd8cf784153edd3886ebc825a0ab1b6043dc
skrolle <>
2004-10-10 11:31:20 +0000

    *** empty log message ***

--- kom/java/nu/rydin/kom/exceptions/LineEditingInterruptedException.java
+++ kom/java/nu/rydin/kom/exceptions/LineEditingInterruptedException.java
@@ -1,16 +1,10 @@
 /*
  * Created on 2004-okt-09
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package nu.rydin.kom.exceptions;
 
 /**
  * @author Magnus Ihse
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class LineEditingInterruptedException extends
         OperationInterruptedException
--- kom/java/nu/rydin/kom/frontend/text/editor/simple/SaveEditorException.java
+++ kom/java/nu/rydin/kom/frontend/text/editor/simple/SaveEditorException.java
@@ -1,8 +1,5 @@
 /*
  * Created on 2004-okt-10
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package nu.rydin.kom.frontend.text.editor.simple;
 
@@ -10,9 +7,6 @@ import nu.rydin.kom.exceptions.SystemException;
 
 /**
  * @author Magnus Ihse
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class SaveEditorException extends SystemException
 {
