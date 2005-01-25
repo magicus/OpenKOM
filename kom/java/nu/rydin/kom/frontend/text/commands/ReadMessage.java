1d906f375fb2de278d24c50197693bb6c8617569
skrolle <>
2005-01-25 18:32:23 +0000

    Hold så op med att brock dig!

--- kom/java/nu/rydin/kom/frontend/text/commands/ReadMessage.java
+++ kom/java/nu/rydin/kom/frontend/text/commands/ReadMessage.java
@@ -13,8 +13,10 @@ import nu.rydin.kom.exceptions.ObjectNotFoundException;
 import nu.rydin.kom.frontend.text.AbstractCommand;
 import nu.rydin.kom.frontend.text.Context;
 import nu.rydin.kom.frontend.text.parser.CommandLineParameter;
+import nu.rydin.kom.frontend.text.parser.ConferenceParameter;
 import nu.rydin.kom.frontend.text.parser.TextNumberParameter;
 import nu.rydin.kom.structs.Envelope;
+import nu.rydin.kom.structs.NameAssociation;
 import nu.rydin.kom.structs.TextNumber;
 
 /**
@@ -24,23 +26,34 @@ public class ReadMessage extends AbstractCommand
 {
 	public ReadMessage(Context context, String fullName)
 	{
-		super(fullName, new CommandLineParameter[] { new TextNumberParameter(true)});	
+		super(fullName, new CommandLineParameter[] { new TextNumberParameter(true), new ConferenceParameter(false) });	
 	}
 	
 	public void execute(Context context, Object[] parameterArray) 
 	throws KOMException
 	{
 	    TextNumber textNum = (TextNumber) parameterArray[0];
+	    
+	    NameAssociation conf = (NameAssociation) parameterArray[1];
 		
 		try
 		{
 			// Retreive message
 			//
 		    ServerSession session = context.getSession();
-			Envelope env = textNum.isGlobal()
-				? session.readGlobalMessage(textNum.getNumber())
-				: session.readLocalMessage((int)textNum.getNumber());
-			
+		    
+		    Envelope env;
+		    if (conf == null)
+		    {
+				env = textNum.isGlobal()
+					? session.readGlobalMessage(textNum.getNumber())
+					: session.readLocalMessage((int)textNum.getNumber());
+		    }
+		    else
+		    {
+		        env = session.readGlobalMessage(textNum.isGlobal()?textNum.getNumber():session.localToGlobal(conf.getId(), (int)textNum.getNumber()));
+		    }
+				
 			// Print it using the default MessagePrinter
 			//
 			context.getMessagePrinter().printMessage(context, env);			
