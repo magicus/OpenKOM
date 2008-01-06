4e5eb70d899b9c0741dc1414b3d302aaebf268bb
mneck <>
2008-01-06 20:02:31 +0000

    Removed unused local variables and imports (removed compiler warnings).

--- kom/java/nu/rydin/kom/frontend/text/commands/FindObject.java
+++ kom/java/nu/rydin/kom/frontend/text/commands/FindObject.java
@@ -7,9 +7,7 @@ import nu.rydin.kom.frontend.text.AbstractCommand;
 import nu.rydin.kom.frontend.text.Context;
 import nu.rydin.kom.frontend.text.DisplayController;
 import nu.rydin.kom.frontend.text.KOMWriter;
-import nu.rydin.kom.frontend.text.LineEditor;
 import nu.rydin.kom.frontend.text.parser.CommandLineParameter;
-import nu.rydin.kom.frontend.text.parser.NamedObjectParameter;
 import nu.rydin.kom.frontend.text.parser.RawParameter;
 import nu.rydin.kom.i18n.MessageFormatter;
 import nu.rydin.kom.structs.NameAssociation;
@@ -27,7 +25,6 @@ public class FindObject extends AbstractCommand
         // Set up
         //
         KOMWriter out = context.getOut();
-        LineEditor in = context.getIn();
         MessageFormatter fmt = context.getMessageFormatter();
         DisplayController dc = context.getDisplayController();
         
