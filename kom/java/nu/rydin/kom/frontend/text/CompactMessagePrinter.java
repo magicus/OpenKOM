97a528b528ebf87e1ad95edd46881c97c1934a04
mneck <>
2008-01-06 20:33:17 +0000

    Refactored MessagePrinter implementation to use common AbstractMessagePrinter for common functions. Changed printing of header and footer to use the same printing functions as BasicMessagePrinter (adds word wrap in header and footer if terminal is too narrow).

--- kom/java/nu/rydin/kom/frontend/text/CompactMessagePrinter.java
+++ kom/java/nu/rydin/kom/frontend/text/CompactMessagePrinter.java
@@ -11,13 +11,11 @@ import java.io.PrintWriter;
 import nu.rydin.kom.backend.data.MessageManager;
 import nu.rydin.kom.constants.ConferencePermissions;
 import nu.rydin.kom.constants.MessageAttributes;
-import nu.rydin.kom.constants.UserFlags;
-import nu.rydin.kom.exceptions.KOMException;
-import nu.rydin.kom.frontend.text.ansi.ANSISequences;
-import nu.rydin.kom.frontend.text.editor.WordWrapper;
+import nu.rydin.kom.exceptions.AuthorizationException;
+import nu.rydin.kom.exceptions.ObjectNotFoundException;
+import nu.rydin.kom.exceptions.UnexpectedException;
 import nu.rydin.kom.i18n.MessageFormatter;
 import nu.rydin.kom.structs.Envelope;
-import nu.rydin.kom.structs.Message;
 import nu.rydin.kom.structs.MessageAttribute;
 import nu.rydin.kom.structs.MessageOccurrence;
 import nu.rydin.kom.structs.NameAssociation;
@@ -26,108 +24,119 @@ import nu.rydin.kom.utils.PrintUtils;
 /**
  * @author Henrik Schröder
  */
-public class CompactMessagePrinter implements MessagePrinter
+public class CompactMessagePrinter extends AbstractMessagePrinter
 {
-	public void printMessage(Context context, Envelope envelope)
-		throws KOMException
-	{
-	    DisplayController dc = context.getDisplayController();
-		PrintWriter out = context.getOut();
-		MessageFormatter formatter = context.getMessageFormatter();
-		Message message = envelope.getMessage();
-		MessageOccurrence primaryOcc = envelope.getPrimaryOccurrence();
-		MessageAttribute[] attributes = envelope.getAttributes();
-		int width = context.getTerminalSettings().getWidth();
-		
-		// Clear screen if requested by user
-		//
-		if(context.isFlagSet(0, UserFlags.CLEAR_SCREEN_BEFORE_MESSAGE))
-			out.print(ANSISequences.CLEAR_DISPLAY);
 
-		// Start printing header
-		//
-		dc.messageHeader();
-		out.print(formatter.format("CompactMessagePrinter.textnumber"));
+    protected void printHeaderReceivers(Context context, Envelope envelope) throws AuthorizationException, ObjectNotFoundException, UnexpectedException 
+    {
+        PrintWriter out = context.getOut();
+        MessageFormatter formatter = context.getMessageFormatter();
+        int width = context.getTerminalSettings().getWidth();
 
-		// If we have a primary occurence, AND if we are in the conference of this 
-		// occurence, then print the local messagenumber, otherwise print the global 
-		// messagenumber. 
-		if((primaryOcc != null) && (primaryOcc.getConference() == context.getSession().getCurrentConferenceId()))
-		{
-			out.print(primaryOcc.getLocalnum());
-		}
-		else
-		{
-			out.print("(");
-			out.print(message.getId());
-			out.print(')');
-		}
-		
-		// Print name of author
+        // Print original occurrence and any moves. This should typically 
+	    // result in only one line, because it is very uncommon to move a copy
+	    // and if that is done, it will be pretty hard to interpret the header anyway. :-)
 		//
-		out.print("; ");
-		out.print(context.formatObjectName(message.getAuthorName(), message.getAuthor()));
-				
-		// Print creation date
-		//
-		out.print("; ");
-		out.println(context.smartFormatDate(message.getCreated()));
-		
-		// Print the original mail recipient if this is a mail.
+        MessageAttribute[] attributes = envelope.getAttributes();
+
+		MessageOccurrence[] occs = envelope.getOccurrences();
+		NameAssociation[] receivers = envelope.getReceivers();
+		for(int idx = 0; idx < receivers.length; ++idx)
+		{
+		    MessageOccurrence occ = occs[idx];
+			// Make sure we only print occurrences in conferences we have read-permission in!
+			//
+			if (context.getSession().hasPermissionInConference(occ.getConference(), ConferencePermissions.READ_PERMISSION))
+			{
+				switch(occ.getKind())
+				{
+					case MessageManager.ACTION_CREATED:
+					    PrintUtils.printIndented(out, formatter.format(getResourceKey("original"), 
+					            context.formatObjectName(receivers[idx].getName(), receivers[idx].getId())),
+						        width, 0);
+					    break;
+					case MessageManager.ACTION_MOVED:
+					    PrintUtils.printIndented(out, formatter.format(getResourceKey("moved"), 
+					            new Object[] { context.formatObjectName(receivers[idx].getName(), receivers[idx].getId()), 
+						            getMovedFrom(attributes), 
+						            context.formatObjectName(occ.getUser()) }),
+						        width, 0);
+						break;					
+				}
+			}
+		}
+    }
+
+
+    protected String getFormattedOriginInfo(Context context, Envelope.RelatedMessage replyTo)
+    {
+        MessageFormatter formatter = context.getMessageFormatter();
+
+        if(replyTo.isLocal())
+        {
+            // Simple case: Original text was in same conference
+            //
+            return formatter.format(getResourceKey("reply.to.same.conference"), 
+                new Object[] { new Long(replyTo.getOccurrence().getLocalnum()), 
+                    context.formatObjectName(replyTo.getAuthorName(), replyTo.getAuthor()) });        
+        }
+        else
+        {
+            // Complex case: Original text was in a different conference
+            //
+            return formatter.format(getResourceKey("reply.to.different.conference"), 
+                new Object[] { new Long(replyTo.getOccurrence().getLocalnum()),
+                    context.formatObjectName(replyTo.getConferenceName(), replyTo.getConference()),
+                    context.formatObjectName(replyTo.getAuthorName(), replyTo.getAuthor()) });
+        }
+
+    }
+
+
+    protected String getFormattedOriginDeletedInfo(Context context, MessageAttribute originalDeletedAttribute)
+    {
+        MessageFormatter formatter = context.getMessageFormatter();
+
+        return formatter.format(getResourceKey("reply.to.deleted.text"), 
+                context.formatObjectName(originalDeletedAttribute.getUsername(), originalDeletedAttribute.getUserId()));
+    }
+    
+    
+    protected void printMailInformation(Context context, Envelope envelope)
+    {
+        PrintWriter out = context.getOut();
+        MessageFormatter formatter = context.getMessageFormatter();
+        int width = context.getTerminalSettings().getWidth();
+
+        MessageAttribute[] attributes = envelope.getAttributes();
+
+        // Print the original mail recipient if this is a mail.
 		//
 		for(int idx = 0; idx < attributes.length; ++idx)
 		{
 		    MessageAttribute each = attributes[idx];
 		    if(each.getKind() == MessageAttributes.MAIL_RECIPIENT && each.getUserId() != context.getLoggedInUserId())
 		    {
-		        out.println(formatter.format("CompactMessagePrinter.original.mail.recipient", context.formatObjectName(each.getUsername(), each.getUserId())));		        
+		        PrintUtils.printIndented(out, formatter.format(getResourceKey("original.mail.recipient"), 
+		                context.formatObjectName(each.getUsername(), each.getUserId())), width, 0);		        
 		    }
 		}
-		
-		// Print reply info (if any)
-		//
-		Envelope.RelatedMessage replyTo = envelope.getReplyTo();
-		if(replyTo != null)
-		{
-			if(replyTo.isLocal())
-			{
-				// Simple case: Original text was in same conference
-				//
-				out.println(formatter.format("CompactMessagePrinter.reply.to.same.conference", 
-					new Object[] { new Long(replyTo.getOccurrence().getLocalnum()), 
-						context.formatObjectName(replyTo.getAuthorName(), replyTo.getAuthor()) } ));		
-			}
-			else
-			{
-				// Complex case: Original text was in a different conference
-				//
-				out.println(formatter.format("CompactMessagePrinter.reply.to.different.conference", 
-					new Object[] { new Long(replyTo.getOccurrence().getLocalnum()),
-				        context.formatObjectName(replyTo.getConferenceName(), replyTo.getConference()),
-				        context.formatObjectName(replyTo.getAuthorName(), replyTo.getAuthor()) }));
-			}
-		}
-		else
-		{
-		    // Even though this text looks like it's not a comment, it might be
-		    // a comment to a deleted text.
-			for(int idx = 0; idx < attributes.length; ++idx)
-			{
-			    MessageAttribute each = attributes[idx];
-			    if(each.getKind() == MessageAttributes.ORIGINAL_DELETED)
-			    {
-			        out.println(formatter.format("BasicMessagePrinter.reply.to.deleted.text", context.formatObjectName(each.getUsername(), each.getUserId())));		        
-			    }
-			}
-		}
-		
-	    // Print original occurence and any moves. This should typically 
-	    // result in only one line, because it is very uncommon to move a copy
-	    // and if that is done, it will be pretty hard to interpret the header anyway. :-)
-		//
-		MessageOccurrence[] occs = envelope.getOccurrences();
-		NameAssociation[] receivers = envelope.getReceivers();
-		String movedFrom = null;
+    }
+
+
+    protected void printFooterReceivers(Context context, Envelope envelope) throws AuthorizationException,
+            ObjectNotFoundException, UnexpectedException
+    {
+        DisplayController dc = context.getDisplayController();
+        PrintWriter out = context.getOut();
+        MessageFormatter formatter = context.getMessageFormatter();
+        int width = context.getTerminalSettings().getWidth();
+
+        dc.messageFooter();
+
+        MessageOccurrence[] occs = envelope.getOccurrences();
+        NameAssociation[] receivers = envelope.getReceivers();
+        
 		int top = receivers.length;
 		for(int idx = 0; idx < top; ++idx)
 		{
@@ -138,146 +147,58 @@ public class CompactMessagePrinter implements MessagePrinter
 			{
 				switch(occ.getKind())
 				{
-					case MessageManager.ACTION_CREATED:
-					    out.println(formatter.format("CompactMessagePrinter.original", 
-						        context.formatObjectName(receivers[idx].getName(), receivers[idx].getId())));
-					    break;
-					case MessageManager.ACTION_MOVED:
-						for(int attrIdx = attributes.length-1; 0 <= attrIdx; --attrIdx)
-						{
-							MessageAttribute each = attributes[attrIdx];
-							if(each.getKind() == MessageAttributes.MOVEDFROM)
-							{
-								movedFrom = new String(each.getValue());
-								break;
-							}
-						}
-						out.println(formatter.format("CompactMessagePrinter.moved", 
-							new Object[] { context.formatObjectName(receivers[idx].getName(), receivers[idx].getId()), 
-						        movedFrom, 
-						        context.formatObjectName(occ.getUser()) }));
-						break;					
-				}
-			}
-		} 
-				
-		// Print subject
-		//
-		String subjLine = formatter.format("BasicMessagePrinter.subject"); 
-		out.print(subjLine);
-		dc.messageSubject();
-		out.println(message.getSubject());
-		dc.messageHeader();
-		PrintUtils.printRepeated(out, '-', subjLine.length() + message.getSubject().length());
-		out.println();
-		
-		// Print body
-		//
-		dc.messageBody();
-		WordWrapper ww = context.getWordWrapper(message.getBody());
-		String line;
-		while((line = ww.nextLine()) != null)
-		{
-		    dc.printWithAttributes(line);
-		    out.println();
-		}
-		out.println();
-		dc.messageFooter();
-		
-		// Print text footer if requested
-		//
-		if(context.isFlagSet(0, UserFlags.SHOW_TEXT_FOOTER))
-		{
-			// If we have a primary occurence, AND if we are in the conference of this 
-			// occurence, then print the local messagenumber, otherwise print the global 
-			// messagenumber. 
-			if((primaryOcc != null) && (primaryOcc.getConference() == context.getSession().getCurrentConferenceId()))
-			{
-				out.println(formatter.format("CompactMessagePrinter.local.footer", 
-				        new Object[] { new Integer(primaryOcc.getLocalnum()), 
-				        	context.formatObjectName(message.getAuthorName(), message.getAuthor()) }));
-			}
-			else
-			{
-				out.println(formatter.format("CompactMessagePrinter.global.footer", 
-				        new Object[] { new Long(message.getId()), 
-				        	context.formatObjectName(message.getAuthorName(), message.getAuthor()) }));
-			}
-		}
-		
-		// Print list of footnotes
-		//
-		for(int idx = 0; idx < attributes.length; ++idx)
-		{
-		    MessageAttribute each = attributes[idx];
-		    if(each.getKind() == MessageAttributes.FOOTNOTE)
-		    {
-		        dc.header();
-		        String label = formatter.format("BasicMessagePrinter.footnote"); 
-		        out.print(label);
-		        dc.messageBody();
-		        int ll = label.length();
-		        PrintUtils.printIndented(
-		                out,
-		                each.getValue(),
-		                width - ll, 0, ll);		        
-		    }
-		}
-
-		// Print copies
-		//
-		top = receivers.length;
-		for(int idx = 0; idx < top; ++idx)
-		{
-		    MessageOccurrence occ = occs[idx];
-			// Make sure we only print occurences in conferences we have read-permission in!
-			//
-			if (context.getSession().hasPermissionInConference(occ.getConference(), ConferencePermissions.READ_PERMISSION))
-			{
-				switch(occ.getKind())
-				{
 					case MessageManager.ACTION_COPIED:
-						out.println(formatter.format("CompactMessagePrinter.copied", 
+					    PrintUtils.printIndented(out, formatter.format(getResourceKey("copied"), 
 							new Object[] { context.formatObjectName(receivers[idx].getName(), receivers[idx].getId()), 
-						        context.formatObjectName(occ.getUser()) }));
+						        context.formatObjectName(occ.getUser()) }),
+						    width, 0);
 						break;					
 				}
 			}
 		}
+    }
+   
+    protected String getFormattedReply(Context context, Envelope.RelatedMessage reply, MessageOccurrence occ)
+    {
+        MessageFormatter formatter = context.getMessageFormatter();
 
-		// Print list of replies
-		//
-		Envelope.RelatedMessage[] replies = envelope.getReplies();
-		top = replies.length;
-		for(int idx = 0; idx < top; ++idx)
-		{
-			Envelope.RelatedMessage each = replies[idx];	
-			MessageOccurrence occ = each.getOccurrence();
-			
-			// Reply in same conference? 
-			//
-			if(each.isLocal())
-			{
-				out.println(formatter.format("CompactMessagePrinter.reply.same.conference",
-					new Object[] { new Long(occ.getLocalnum()), each.getAuthorName() }));
-			}
-			else
-			{
-				out.println(formatter.format("CompactMessagePrinter.reply.different.conference",
-					new Object[] { new Long(occ.getLocalnum()), each.getConferenceName(), 
-						each.getAuthorName() }));
-			}
-		}
+        // Reply in same conference? 
+        //
+        if(reply.isLocal())
+        {
+            return formatter.format(getResourceKey("reply.same.conference"),
+                new Object[] { new Long(occ.getLocalnum()), reply.getAuthorName() });
+        }
+        else
+        {
+            return formatter.format(getResourceKey("reply.different.conference"),
+                new Object[] { new Long(occ.getLocalnum()), reply.getConferenceName(), 
+                    reply.getAuthorName() });
+        }
+    }
+    
 
-		// Print list of "no comments"
-		//
-		for(int idx = 0; idx < attributes.length; ++idx)
-		{
-		    MessageAttribute each = attributes[idx];
-		    if(each.getKind() == MessageAttributes.NOCOMMENT)
-		    {
-				out.println(formatter.format("BasicMessagePrinter.nocomment", context.formatObjectName(each.getUsername(), each.getUserId())));
-		    }
-		}
-	}
+    protected StringBuffer getFormattedMessageId(Context context, Envelope envelope)
+    {
+        MessageOccurrence primaryOcc = envelope.getPrimaryOccurrence();
+        
+        StringBuffer sb = new StringBuffer();
+        
+        // If we have a primary occurence, AND if we are in the conference of this 
+        // occurence, then print the local messagenumber, otherwise print the global 
+        // messagenumber. 
+        if((primaryOcc != null) && 
+                (primaryOcc.getConference() == context.getSession().getCurrentConferenceId()))
+        {
+            sb.append(String.valueOf(primaryOcc.getLocalnum()));
+        }
+        else
+        {
+            sb.append('(');
+            sb.append(envelope.getMessage().getId());
+            sb.append(')');
+        }
+        return sb;
+    }
+
 }
