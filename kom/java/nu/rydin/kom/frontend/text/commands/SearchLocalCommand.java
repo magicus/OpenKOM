ecfaf3220c97a2c21ea689cde92f966c0c6fb4a7
mneck <>
2008-01-09 22:36:10 +0000

    SearchLocal commands should show sender/receiver in the mailbox (and not the author when it's the current user). Moved innerPrintSearchResultRow and printSearchResultHeader implementation to ConferenceMessageSearchResultPrinter class.

--- kom/java/nu/rydin/kom/frontend/text/commands/SearchLocalCommand.java
+++ kom/java/nu/rydin/kom/frontend/text/commands/SearchLocalCommand.java
@@ -6,15 +6,11 @@
  */
 package nu.rydin.kom.frontend.text.commands;
 
+import nu.rydin.kom.exceptions.UnexpectedException;
 import nu.rydin.kom.frontend.text.Context;
-import nu.rydin.kom.frontend.text.DisplayController;
 import nu.rydin.kom.frontend.text.KOMWriter;
 import nu.rydin.kom.frontend.text.parser.CommandLineParameter;
-import nu.rydin.kom.i18n.MessageFormatter;
-import nu.rydin.kom.structs.LocalMessageSearchResult;
 import nu.rydin.kom.structs.MessageSearchResult;
-import nu.rydin.kom.utils.HeaderPrinter;
-import nu.rydin.kom.utils.PrintUtils;
 
 /**
  * @author Henrik Schröder
@@ -26,50 +22,33 @@ public abstract class SearchLocalCommand extends SearchCommand
     protected static final int AUTHOR_COL_WIDTH = 30;
 
     protected static final int SUBJECT_COL_WIDTH = 28;
+    
+    protected MessageSearchResultPrinter m_resultPrinter = null;
 
     public SearchLocalCommand(String fullName, CommandLineParameter[] signature, long permissions)
     {
         super(fullName, signature, permissions);
     }
 
+    protected void preparePrinting(Context context) 
+    {
+        if (context.getSession().getCurrentConferenceId() == context.getLoggedInUserId()) {
+            m_resultPrinter = new MailboxMessageSearchResultPrinter();
+        }
+        else
+        {
+            m_resultPrinter = new ConferenceMessageSearchResultPrinter();
+        }
+    }
+    
     protected void innerPrintSearchResultRow(Context context, KOMWriter out,
             MessageSearchResult msr)
     {
-        LocalMessageSearchResult lmsr = (LocalMessageSearchResult) msr;
-        DisplayController dc = context.getDisplayController();
-        dc.normal();
-        PrintUtils.printRightJustified(out, String.valueOf(lmsr.getLocalId()),
-                LOCALID_COL_WIDTH);
-        out.print(" ");
-        dc.output();
-        PrintUtils.printLeftJustified(out, context.formatObjectName(lmsr
-                .getAuthor()), AUTHOR_COL_WIDTH);
-        out.print(" ");
-		dc.header();
-		out.print(msr.getReplyTo() > 0 ? "  " : "* ");
-		dc.output();        
-        PrintUtils
-                .printLeftJustified(out, lmsr.getSubject(), SUBJECT_COL_WIDTH);
-        out.println();
-        dc.normal();
-        out.flush();
+        m_resultPrinter.printSearchResultRow(context, out, msr);        
     }
     
     protected void printSearchResultHeader(Context context)
     {
-        KOMWriter out = context.getOut();
-        MessageFormatter formatter = context.getMessageFormatter();
-        DisplayController dc = context.getDisplayController();
-        dc.normal();
-        HeaderPrinter hp = new HeaderPrinter();
-        hp.addHeader(formatter.format("search.heading.text"),
-                LOCALID_COL_WIDTH, true);
-        hp.addSpace(1);
-        hp.addHeader(formatter.format("search.heading.writtenby"),
-                AUTHOR_COL_WIDTH, false);
-        hp.addSpace(3);
-        hp.addHeader(formatter.format("search.heading.subject"),
-                SUBJECT_COL_WIDTH, false);
-        hp.printOn(out);
+        m_resultPrinter.printSearchResultHeader(context);
     }
 }
