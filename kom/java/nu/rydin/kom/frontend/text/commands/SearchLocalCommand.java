c24222c3739e8fa7349e7af3c88e51d0fb0c3274
mneck <>
2008-01-10 00:29:04 +0000

    Removed unused import.

--- kom/java/nu/rydin/kom/frontend/text/commands/SearchLocalCommand.java
+++ kom/java/nu/rydin/kom/frontend/text/commands/SearchLocalCommand.java
@@ -6,7 +6,6 @@
  */
 package nu.rydin.kom.frontend.text.commands;
 
-import nu.rydin.kom.exceptions.UnexpectedException;
 import nu.rydin.kom.frontend.text.Context;
 import nu.rydin.kom.frontend.text.KOMWriter;
 import nu.rydin.kom.frontend.text.parser.CommandLineParameter;
