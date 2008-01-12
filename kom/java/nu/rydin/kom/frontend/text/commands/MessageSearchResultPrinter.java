a489683e27a79df0696338846a59128eed64db4c
mneck <>
2008-01-12 19:38:02 +0000

    Added missing file header.

--- kom/java/nu/rydin/kom/frontend/text/commands/MessageSearchResultPrinter.java
+++ kom/java/nu/rydin/kom/frontend/text/commands/MessageSearchResultPrinter.java
@@ -1,9 +1,18 @@
+/*
+ * Created on Jan 9, 2008
+ *  
+ * Distributed under the GPL license.
+ * See http://www.gnu.org for details
+ */
 package nu.rydin.kom.frontend.text.commands;
 
 import nu.rydin.kom.frontend.text.Context;
 import nu.rydin.kom.frontend.text.KOMWriter;
 import nu.rydin.kom.structs.MessageSearchResult;
 
+/**
+ * @author <a href=mailto:magnus.neck@abc.se>Magnus Neck</a>
+ */
 public interface MessageSearchResultPrinter
 {
 
