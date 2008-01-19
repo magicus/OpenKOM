191b48838244c2c3dbfc9fc7242f9bb48fdf9fd9
jepson <>
2008-01-19 15:05:33 +0000

    Quick hack: copying from closed or exclusive confs now require confirmation.

--- kom/java/nu/rydin/kom/frontend/text/commands/Copy.java
+++ kom/java/nu/rydin/kom/frontend/text/commands/Copy.java
@@ -7,16 +7,22 @@
 package nu.rydin.kom.frontend.text.commands;
 
 import java.io.PrintWriter;
+import java.io.IOException;
 
 import nu.rydin.kom.backend.ServerSession;
+import nu.rydin.kom.constants.ConferencePermissions;
+import nu.rydin.kom.constants.Visibilities;
 import nu.rydin.kom.exceptions.KOMException;
 import nu.rydin.kom.exceptions.NoCurrentMessageException;
+import nu.rydin.kom.exceptions.OperationInterruptedException;
 import nu.rydin.kom.frontend.text.AbstractCommand;
 import nu.rydin.kom.frontend.text.Context;
 import nu.rydin.kom.frontend.text.DisplayController;
+import nu.rydin.kom.frontend.text.LineEditor;
 import nu.rydin.kom.frontend.text.parser.CommandLineParameter;
 import nu.rydin.kom.frontend.text.parser.NamedObjectParameter;
 import nu.rydin.kom.i18n.MessageFormatter;
+import nu.rydin.kom.structs.ConferenceInfo;
 import nu.rydin.kom.structs.MessageOccurrence;
 import nu.rydin.kom.structs.NameAssociation;
 
@@ -31,7 +37,7 @@ public class Copy extends AbstractCommand
 	}
 	
 	public void execute(Context context, Object[] parameterArray) 
-	throws KOMException, NoCurrentMessageException
+	throws KOMException, IOException, InterruptedException, NoCurrentMessageException
 	{
 	    ServerSession session = context.getSession();
 		PrintWriter out = context.getOut();
@@ -43,7 +49,30 @@ public class Copy extends AbstractCommand
 		long messageid = session.getCurrentMessage();
 		if(messageid == -1)
 			throw new NoCurrentMessageException();
-	    
+
+        ConferenceInfo ci = session.getCurrentConference();
+        boolean mustConfirm = ((ci.getPermissions() & ConferencePermissions.READ_PERMISSION) == 0) || (ci.getVisibility() != Visibilities.PUBLIC);
+        if (mustConfirm)
+        {
+            LineEditor in = context.getIn();
+            int choice = in.getChoice(fmt.format("copy.confirm.leak",
+                    ci.getName().getName()) +
+                    " (" +
+                    fmt.format("misc.yes") +
+                    "/" + 
+                    fmt.format("misc.no") +
+                    ")? ",
+                    new String[] { fmt.format("misc.no"), 
+                                   fmt.format("misc.yes") },
+                    0,
+                    fmt.format("nu.rydin.kom.exceptions.InvalidChoiceException.format"));
+            
+            if (1 != choice)
+            {
+                throw new OperationInterruptedException();
+            }
+        }
+        
 	    long destination = ((NameAssociation)parameterArray[0]).getId();
 
 		// Call backend
