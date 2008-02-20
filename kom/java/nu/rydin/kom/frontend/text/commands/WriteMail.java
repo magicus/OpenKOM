5defbc460d80f2d226f00a9c85e741cb598c9e63
jepson <>
2008-02-20 01:13:00 +0000

    Fixed bug #1039542, "note isn't shown when mailing user".

--- kom/java/nu/rydin/kom/frontend/text/commands/WriteMail.java
+++ kom/java/nu/rydin/kom/frontend/text/commands/WriteMail.java
@@ -7,14 +7,19 @@
 package nu.rydin.kom.frontend.text.commands;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import nu.rydin.kom.backend.ServerSession;
 import nu.rydin.kom.constants.Activities;
+import nu.rydin.kom.exceptions.AuthorizationException;
 import nu.rydin.kom.exceptions.KOMException;
+import nu.rydin.kom.exceptions.ObjectNotFoundException;
 import nu.rydin.kom.frontend.text.AbstractCommand;
 import nu.rydin.kom.frontend.text.Context;
 import nu.rydin.kom.frontend.text.MessageEditor;
 import nu.rydin.kom.frontend.text.parser.CommandLineParameter;
 import nu.rydin.kom.frontend.text.parser.NamedObjectParameter;
+import nu.rydin.kom.i18n.MessageFormatter;
 import nu.rydin.kom.structs.MessageOccurrence;
 import nu.rydin.kom.structs.NameAssociation;
 import nu.rydin.kom.structs.UnstoredMessage;
@@ -33,14 +38,42 @@ public class WriteMail extends AbstractCommand
     public void execute(Context context, Object[] parameterArray)
             throws KOMException, IOException, InterruptedException
     {
+        ServerSession ss = context.getSession();
+        
         // Get parameters
         //
         NameAssociation recipient = (NameAssociation) parameterArray[0];
 
         // Update state
         //
-        context.getSession().setActivity(Activities.MAIL, true);
-        context.getSession().setLastObject(recipient.getId());
+        ss.setActivity(Activities.MAIL, true);
+        ss.setLastObject(recipient.getId());
+        
+        try
+        {
+            // Has the user left a note? If so, print it. 
+            //
+            PrintWriter out = context.getOut();
+            String note = context.getSession().readFile(recipient.getId(), ".note.txt");
+            String[] lines = note.split("\n");
+
+            MessageFormatter mf = context.getMessageFormatter();
+            out.println(mf.format("status.user.note"));
+            for (int i = 0; i < lines.length; ++i)
+            {
+                out.println(lines[i]);
+            }
+            out.println();
+        }
+        catch (ObjectNotFoundException e)
+        {
+            // No note, so nothing to print
+        }
+        catch (AuthorizationException e)
+        {
+            // This shouldn't even happen in a user situation. Even IF it happens, we
+            // still have nothing to print and so fail silently.
+        }
         
         // Get editor and execute it
         //
